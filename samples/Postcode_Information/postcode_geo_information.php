bf38b31f23cd0da4f6745965feec37f2ba69df31
Moussa Ball <moiseball20155@gmail.com>
2022-08-25 13:32:08 +0000

    Rename .PHP to .php

--- samples/Postcode_Information/postcode_geo_information.php
+++ samples/Postcode_Information/postcode_geo_information.php
@@ -4,7 +4,7 @@ require "../bootstrap.php";
 
 use AddressesAndPostcodes\Lookup\PHP\SDK\Client;
 
-$client = new Client('1616589158-361070052-462388701-1635564164');
+$client = new Client('1317582431-899674233-1600765198-702694870');
 $output = $client->postcodeInformation()->postcodeGEOInformation('AB10 1AB');
 
 echo '<pre>';
